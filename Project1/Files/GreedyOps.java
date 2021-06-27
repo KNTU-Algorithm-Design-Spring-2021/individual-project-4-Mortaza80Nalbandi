e2bf56da1771e5e9400cc158bb3c4729704a2586
Mortaza Nalbandi <mortazana8@gmail.com>
2021-06-27 19:42:10 +0430

    done the basics for project 1

---
+++ Project1/Files/GreedyOps.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class GreedyOps {
+    private  int n, k, maxW;
+    private int[] w;
+    public GreedyOps(int n, int k,int[] w,int maxW){
+        this.n = n;
+        this.k=k;
+        this.w=w;
+        this.maxW = maxW;
+    }
+    public int greedyOp1(){
+        return 0;
+    }
+    public int greedyOp2(){
+        return 0;
+    }
+}
---
+++ Project1/Files/PostCompany.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.lang.Math;
+ public class PostCompany{
+     public static void main(String[] args ){
+         Scanner scanner = new Scanner(System.in);
+         System.out.println("Enter number of Items : ");
+         int n = scanner.nextInt();
+         System.out.println("Enter number of Trucks : ");
+         int k = scanner.nextInt();
+         System.out.println("Enter max Weight ligal : ");
+         int maxW = scanner.nextInt();
+         int[] w = new int[n];
+         System.out.println("Enter Weights of items : ");
+         for(int i=0;i<n;i++){
+             w[i] = scanner.nextInt();
+         }
+         GreedyOps go = new GreedyOps(n,k,w,maxW);
+         System.out.println("Max possible weight for a truck is equal : " + Math.max(go.greedyOp1(),go.greedyOp2()));
+     }
+ }
